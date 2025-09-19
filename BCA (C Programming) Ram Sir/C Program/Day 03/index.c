f5a84ddfff55ddc55d7a2b64e9b7550d28a523a1
rohitshrivastav-official <rohitshrivastav0675@gmail.com>
2025-09-19 13:59:25 +0530

    ram sir c homework 01 added

---
+++ BCA (C Programming) Ram Sir/C Program/Day 03/index.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    float f;
+    float g;
+    
+    printf("Enter 1st number");
+    scanf("%d",&a);
+
+    printf("Enter 2nd number");
+    scanf("%d",&b);
+//incomplete code it can change soon
+    c=a+b;
+    printf(" %d",c);
+    d=a-b;
+    printf(" %d",d);
+    e=a*b;
+    printf(" %d",e);
+    f=a/b;
+    printf(" %f",f);
+    g=a%b;
+    printf(" %f",g);
+}
